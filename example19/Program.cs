d3932295cd8f5e6ebd3e93ae762d95f1cbc6b263
Valery <wogl@mail.ru>
2022-11-20 13:02:59 +0300

    added example 13 15 17 19

--- example19/Program.cs
+++ example19/Program.cs
@@ -38,3 +38,34 @@ PrintArray(arr);
 SelectionSort(arr);
 
 PrintArray(arr);
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 }; //кто-то отредактировал типа недочеты из конспекта
+// void PrintArray(int[] array)
+// {
+// int count = array.Length;
+// for (int i = 0; i < count; i++)
+// {
+// Console.Write($"{array[i]}");
+// }
+// Console.WriteLine();
+// }
+// void selectionSort(int[] array)
+// {
+// for (int i = 0; i < array.Length - 1; i++)
+// {
+// int minPosition = i;
+// for (int j = i + 1; j < array.Length; j++)
+// {
+// if (array[j] < array[minPosition]) minPosition = j;
+// {
+// minPosition = j;
+// }
+// }
+// int temporary = array[i];
+// array[i] = array[minPosition];
+// array[minPosition] = temporary;
+// }
+// }
+// PrintArray(arr);
+// selectionSort(arr);
+// PrintArray(arr);
