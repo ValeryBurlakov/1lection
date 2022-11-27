cfa715e26cd3fc3181018ec4db63e83a687d2fdb
Valery <wogl@mail.ru>
2022-11-27 14:55:09 +0300

    20 21

---
+++ example20/Program.cs
@@ -0,0 +1,46 @@
+﻿string[,] table = new string[2, 5];// двумерный массив
+// String.empty
+// table[0, 0]  table[0, 1] table[0, 2]..... table[0, 4]
+// table[1, 0]  table[1, 1] table[1, 1] table[1, 2]
+
+// table[1, 2] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+void PrintArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)// matrix.GetLength(0) выдаёт количество строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)// matrix.GetLength(1) выдаёт количество столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); // [1, 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4]; // 3 строки, 4 столбца
+
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
---
+++ example22/Program.cs
@@ -0,0 +1,49 @@
+﻿// рекурсия
+
+// 1. факториал можно решить рекурсией
+
+// double Factorial(int n)
+// {
+//    // 1! = 1
+//    //  0! = 1
+//      if (n == 1) return 1;
+//      else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");//1*2*3 = 6
+// }
+// при таком коде переполняется тип данных integer на 17 и появляется отрицательный знак
+// для решения в 3 строке мы применили тип double, который смог переварить такие большие числа
+
+// 2. Числа фибоначчи
+// Fn = Fn-1 + Fn-2
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+// int Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2); // return потому что нам нужно вернуть значение
+// }
+
+// for(int i = 1; i < 10; i++)
+// {
+//     Console.WriteLine(Fibonacci(i));
+// }
+
+
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2); // return потому что нам нужно вернуть значение
+// }
+
+// for(int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f{i} = {Fibonacci(i)}");
+// }
+// очень медленно работает
+
+// 3. Обход директорию рекурсией
